897b097088a1203c38f36dc9b48f27ad766364f1
schneidan <dan@scrollwright.com>
2016-02-02 10:24:24 -0700

    Remove unused references to missing unveil plugin

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,6 @@ var body = document.body, html = document.documentElement;
 var docHeight = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 var current = '';
 var swapped = false;
-var galleriesLoaded = [];
 var adsloaded = [];
 /* THIS IS CONFIG DATA SPECIFIC TO SITE */
 var showAds = true; //show slide-up leaderboards at bottom
@@ -13,9 +12,7 @@ var titleFade = true; //whether to fade the Denver Post logo in the top-bar to s
 //var pages = ['#titlepage','#part1','#photos','#part2']; //div/section IDs that should trigger a page view and title change
 var pages = [];
 $('.omnitrig').each(function(i,e) { pages.push('#'+$(e).attr('id')) });
-var galleries = [];
 var playedVideos = [];
-$('.centergallery').each(function(i,e) { galleries.push('#'+$(e).attr('id')) }); //div/section IDs of galleries to instantiate (must be a div like #photos and have a child, the gallery itself, with the same ID plus 'gallery' -- i.e. #photosgallery)
 
 function revealSocial(type,link,title,image,desc,twvia,twrel) {
     title = typeof title !== 'undefined' ? title : false;
@@ -61,33 +58,8 @@ $(document).foundation('reveal', {
 function revealCredits() {
     $('#credits').foundation('reveal', 'open');
 }
-function revealSlides(galleries) {
-    for (key in galleries) {
-        if (galleriesLoaded.indexOf(galleries[key]) == -1) {
-            $(galleries[key]).find('img').unveil();
-            $(galleries[key]).slick({
-                centerMode: true,
-                centerPadding: '15%',
-                slidesToShow: 1,
-                prevArrow: '<button type="button" class="slick-prev"><span>&lt;</span></button>',
-                nextArrow: '<button type="button" class="slick-next"><span>&gt;</span></button>',
-                responsive: [{
-                    breakpoint: 800,
-                    settings: {
-                        arrows: true,
-                        centerMode: true,
-                        centerPadding: '8%',
-                        slidesToShow: 1
-                    }
-                }]
-            });
-            galleriesLoaded.push(galleries[key]);
-        }
-    }
-}
 function checkHash() {
     if (window.location.hash) {
-        revealSlides(galleries);
         var hash = window.location.hash;
         if ($(hash).hasClass('hide')) {
             toggleSidebar(hash,hash + 'link');
@@ -285,8 +257,6 @@ function checkPageState(pages) {
     }
 }
 
-$('.chart-late').find('img').unveil(300);
-
 $(document).ready(function() {
     checkHash();
     checkAdPos();
@@ -300,7 +270,6 @@ setInterval(function() {
     if (didScroll) {
         checkFade();
         checkPageState(pages);
-        revealSlides(galleries);
         checkAdPos();
     }
 },250);
